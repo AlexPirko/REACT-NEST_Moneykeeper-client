c72469dd1f79229b8e4a091b032894c7f67347f7
Alex Pirko <youzhny_foto@gmail.com>
2023-10-09 14:53:03 +0300

    feat: add server URL

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { getTokenFromLocalStorage } from '../helpers/localstorage.helper';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    // baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://moneykp-server.onrender.com/api',
     headers: {
         Authorization: `Bearer ` + getTokenFromLocalStorage(),
     },
